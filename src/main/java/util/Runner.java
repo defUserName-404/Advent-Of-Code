9a49f29a59d11a00a9aa18a7f72a542c0619cbf2
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-03-31 03:49:22 +0600

    Runner class for soltions

--- src/main/java/util/Runner.java
+++ src/main/java/util/Runner.java
@@ -1,2 +1,9 @@
-package util;public class Runner {
+package util;
+
+import solution.year_2021.Day1_SonarSweep;
+
+public class Runner {
+	public static void main(String[] args) {
+		Day1_SonarSweep.part1();
+	}
 }
