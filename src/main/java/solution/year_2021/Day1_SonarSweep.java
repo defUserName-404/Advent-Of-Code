c28904d71de4d2c629d64dc4d7db24c7d7f91146
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-03-31 03:51:18 +0600

    Solved and tested 2021 day1- part1

--- src/main/java/solution/year_2021/Day1_SonarSweep.java
+++ src/main/java/solution/year_2021/Day1_SonarSweep.java
@@ -1,2 +1,22 @@
-package solution.year_2021;public class Day1_SonarSweep {
+package solution.year_2021;
+
+import util.IO;
+
+import java.util.ArrayList;
+
+public class Day1_SonarSweep {
+	public static int part1() {
+		ArrayList<String> input = IO.getInstance().readFile();
+		int increaseCount = 0;
+		int prev = Integer.parseInt(input.get(0));
+
+		for (final String num : input) {
+			int currentNum = Integer.parseInt(num);
+			if (currentNum > prev)
+				increaseCount++;
+			prev = currentNum;
+		}
+		IO.getInstance().writeToFile(String.valueOf(increaseCount));
+		return increaseCount;
+	}
 }
--- src/test/java/solution/year_2021/Day1_SonarSweepTest.java
+++ src/test/java/solution/year_2021/Day1_SonarSweepTest.java
@@ -1,6 +1,12 @@
 package solution.year_2021;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class Day1_SonarSweepTest {
-  
+	@Test
+	public void part1() {
+		int ans = Day1_SonarSweep.part1();
+		Assertions.assertEquals(ans, 7);
+	}
 }
