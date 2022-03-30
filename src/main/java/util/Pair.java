4def7fa9f230999ebbdcec55c8a87b1766e1d491
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-03-30 19:08:35 +0600

    Initial commit

---
+++ src/main/java/util/Pair.java
@@ -0,0 +1,25 @@
+package util;
+
+public record Pair<F, S>(F first, S second) {
+	public static <F, S> Pair<F, S> of(F a, S b) {
+		return new Pair<>(a, b);
+	}
+
+	@Override
+	public int hashCode() {
+		return 31 * first.hashCode() + second.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+
+		if (o == null || getClass() != o.getClass()) return false;
+
+		Pair<?, ?> pair = (Pair<?, ?>) o;
+
+		if (!first.equals(pair.first)) return false;
+
+		return second.equals(pair.second);
+	}
+}
