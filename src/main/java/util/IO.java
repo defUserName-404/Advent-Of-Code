5824868bd01dda35a7d881411e3fab883cd0cd04
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-03-31 03:48:54 +0600

    File reader and writer created

--- src/main/java/util/IO.java
+++ src/main/java/util/IO.java
@@ -1,2 +1,43 @@
-package util;public class Files {
+package util;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class IO {
+	private static IO instance;
+	private static final File INPUT_FILE = new File("src/main/resources/io/input.txt");
+	private static final File OUTPUT_FILE = new File("src/main/resources/io/output.txt");
+
+	private IO() {
+	}
+
+	public static IO getInstance() {
+		if (instance == null)
+			instance = new IO();
+		return instance;
+	}
+
+	public ArrayList<String> readFile() {
+		ArrayList<String> inputData = new ArrayList<>();
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(INPUT_FILE));
+			String line;
+			while ((line = reader.readLine()) != null)
+				inputData.add(line);
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return inputData;
+	}
+
+	public void writeToFile(String string) {
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(OUTPUT_FILE));
+			writer.write(string);
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
